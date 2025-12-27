95f804bbb6f95262ad46f6062a23a0042c4bf70e
Chetan <chetanzagade28@gmail.com>
2025-12-27 18:17:33 +0530

    change in server.js

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require("sequelize");
-const sequelize = require("../config/database");
+const sequelize = require("../config/Database");
 const bcrypt = require("bcrypt");
 
 const User = sequelize.define("User", {
